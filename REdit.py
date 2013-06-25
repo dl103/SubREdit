657f2ccec1f182a8b10a21933f45e478ab9d99b6
David Le <d13.broke.it@gmail.com>
2013-06-25 16:25:37 -0400

    Files now retain their original name once opened in local machine; rudimentary save functions

--- REdit.py
+++ REdit.py
@@ -2,6 +2,7 @@ import sublime, sublime_plugin
 import paramiko
 import tempfile
 import time
+import os
 
 class RemoteOpenCommand(sublime_plugin.WindowCommand):
 
@@ -10,7 +11,6 @@ class RemoteOpenCommand(sublime_plugin.WindowCommand):
         pass
 
     def on_done(self, text):
-        print("You wrote " + text)
         s = sublime.load_settings("REditPreferences.sublime-settings")
     	host = s.get("ssh_host")
     	username = s.get("ssh_username")
@@ -18,22 +18,46 @@ class RemoteOpenCommand(sublime_plugin.WindowCommand):
         client = paramiko.SSHClient()
     	client.load_system_host_keys()
     	client.connect(host, username = username, password = pw)
+        self.sftp = client.open_sftp()
+        self.read_file(text)
+        client.close()
+        pass
+
+    def read_file(self, filepath):
+        try:
+            remotefile = self.sftp.open(filepath, mode='r')
+            temp_dir = tempfile.mkdtemp(prefix='REdit-')
+        except IOError as e:
+            sublime.error_message("Could not find file %s" % filepath)
+            return
+        except OSError as e:
+           sublime.error_message("Failed to create a temporary directory! Error: %s" % e)
+           return
+        file_name = filepath.split('/')[-1]
+        local_path = os.path.join(temp_dir, file_name)
+        local_file = open(local_path, "w+")
+        file_contents = remotefile.read()
+        local_file.write(file_contents)
+        local_file.flush()
+        local_file.seek(0)
+        self.window.open_file(local_path)
+
+class RemoteSaveCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        self.window.show_input_panel("Remote File Path:", "", self.on_done, None, None)
+        #print(self.view.substr(sublime.Region(0, self.view.size())))
+
+    def on_done(self, text):
+        print("You wrote " + text)
+        s = sublime.load_settings("REditPreferences.sublime-settings")
+        host = s.get("ssh_host")
+        username = s.get("ssh_username")
+        pw = s.get("ssh_pass")
+        client = paramiko.SSHClient()
+        client.load_system_host_keys()
+        client.connect(host, username = username, password = pw)
         sftp = client.open_sftp()
         remotefile = sftp.open(text, mode='r')
         self.read_file(remotefile)
         client.close()
-        pass
-
-    def read_file(self, file):
-        try:
-           temp_file = tempfile.NamedTemporaryFile(prefix='REdit-')
-        except OSError as e:
-        	sublime.error_message("Failed to create a temporary file! Error: %s" % e)
-        file_contents = file.read()
-        print("Remote file contents: " + file_contents)
-        temp_file.write(file_contents)
-        temp_file.seek(0)
-        #temp_file.flush()
-        print("Temporary file contents: " + temp_file.read())
-        self.window.open_file(temp_file.name)
-        
